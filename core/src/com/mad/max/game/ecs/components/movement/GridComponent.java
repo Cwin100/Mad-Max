943832f5aa3d48dd8d1cced7c35b024144f07372
Cole Winsor <ccwinsor@wpi.edu>
2022-10-20 19:33:43 -0600

    Added GridComponent which snaps mouse moved entities to the grid of its carrier

---
+++ core/src/com/mad/max/game/ecs/components/movement/GridComponent.java
@@ -0,0 +1,7 @@
+package com.mad.max.game.ecs.components.movement;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class GridComponent implements MoveComponent{
+
+}
--- core/src/com/mad/max/game/ecs/systems/movesystems/MouseMoveSystem.java
+++ core/src/com/mad/max/game/ecs/systems/movesystems/MouseMoveSystem.java
@@ -8,8 +8,12 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.mad.max.game.ecs.components.ClickComponent;
 import com.mad.max.game.ecs.components.TransformComponent;
+import com.mad.max.game.ecs.components.movement.CarriedComponent;
+import com.mad.max.game.ecs.components.movement.GridComponent;
 import com.mad.max.game.ecs.components.movement.MouseMoveComponent;
+import com.mad.max.game.ecs.entity.actors.TargetPosition;
 import com.mad.max.game.ecs.systems.LayerComparator;
+import com.mad.max.game.managers.GridManager;
 import com.mad.max.game.managers.MouseManager;
 import com.mad.max.game.managers.SelectionManager;
 
@@ -18,10 +22,13 @@ import java.awt.geom.Line2D;
  public class MouseMoveSystem extends SortedIteratingMoveSystem {
 
     private final ComponentMapper<MouseMoveComponent> mouseMoveM = ComponentMapper.getFor(MouseMoveComponent.class);
-    private final ComponentMapper<TransformComponent> transformM = ComponentMapper.getFor(TransformComponent.class);
+     private final ComponentMapper<TransformComponent> transformM = ComponentMapper.getFor(TransformComponent.class);
+     private final ComponentMapper<GridComponent> gridM = ComponentMapper.getFor(GridComponent.class);
+     private final ComponentMapper<CarriedComponent> carriedM = ComponentMapper.getFor(CarriedComponent.class);
 
     private MouseManager mm = MouseManager.get();
-    private SelectionManager sm = SelectionManager.get();
+     private SelectionManager sm = SelectionManager.get();
+     private GridManager gm = GridManager.get();
 
     public MouseMoveSystem() {
         super(Family.all(MouseMoveComponent.class, TransformComponent.class).get(), new LayerComparator());
@@ -31,12 +38,11 @@ import java.awt.geom.Line2D;
     protected Vector2 calculateMoveDelta(Entity entity, float deltaTime) {
         TransformComponent transform = transformM.get(entity);
         MouseMoveComponent mouseMove = mouseMoveM.get(entity);
+
         Vector2 moveDelta = new Vector2(0, 0);
 
         Rectangle boundary = new Rectangle(transform.position.x, transform.position.y, transform.size.x, transform.size.y);
         Vector2 mousePos = mm.getPos();
-        //Gdx.app.log("MouseMoveSystem","" + mousePos + "|" + boundary);
-        //Gdx.app.log("MouseMoveSystem", "" + boundary.contains(mousePos));
 
         if(mm.leftClick() && boundary.contains(mousePos)) {
             Gdx.app.log("MouseMoveSystem", "Entity selected");
@@ -44,26 +50,55 @@ import java.awt.geom.Line2D;
         }
         else if(mm.rightClick() && sm.getSelected() == entity) {
             Gdx.app.log("MouseMoveSystem", "Entity directed");
-            mouseMove.targetPosition = mousePos;
+            Vector2 targetPos = mousePos;
+
+            CarriedComponent carried = carriedM.get(entity);
+            GridComponent grid = gridM.get(entity);
+            if(grid != null){//Align to grid if it isn't already
+                Vector2 offsetPixels = new Vector2(0, 0);
+
+                if(carried != null){// If this is carrier, we grid align based on the carrier's grid
+                    TransformComponent referenceObject = transformM.get(carried.getCarrier());
+
+                    offsetPixels.add(referenceObject.position.x % gm.getWidth(), referenceObject.position.y % gm.getHeight());
+                }
+                Gdx.app.log("Grid Align", "" + offsetPixels);
+                targetPos.sub(offsetPixels);
+
+                Vector2 targetGridPosition = new Vector2((int)targetPos.x / gm.getWidth() + .5f, (int)targetPos.y / gm.getHeight() + .5f);
+                Gdx.app.log("Grid Alignment", "GridPos " + targetGridPosition);
+                if((transform.size.x -1) % (2 * gm.getWidth()) > gm.getWidth()){
+                    targetGridPosition.add(.5f, 0);
+                }
+                if((transform.size.y - 1) % (2 * gm.getHeight()) > gm.getHeight()){
+                    targetGridPosition.add(0, .5f);
+                }
+                targetPos = new Vector2(targetGridPosition.x * gm.getWidth(), targetGridPosition.y * gm.getHeight());
+                targetPos.add(offsetPixels);
+                Gdx.app.log("Grid Alignment", "" + targetPos + "|" + mousePos + "|" + targetGridPosition);
+
+            }
+
+            mouseMove.targetPosition = new TargetPosition(targetPos.x, targetPos.y);
+            if(carried != null){
+                //Move the target based on the carrier
+                mouseMove.targetPosition.addMovement(new CarriedComponent(mouseMove.targetPosition, carried.getCarrier()));
+            }
         }
 
         if(mouseMove.targetPosition != null){
-            double directionOfTravel = new Vector2(mouseMove.targetPosition).sub(transform.getCenter()).angleRad();
-            Gdx.app.log("MouseMoveSystem", "Movement angle: " + Math.toDegrees(directionOfTravel));
-
-            double pointDistance = distance(mouseMove.targetPosition, transform.getCenter());
+            Vector2 targetPos = transformM.get(mouseMove.targetPosition).position;
+            double directionOfTravel = new Vector2(targetPos).sub(transform.getCenter()).angleRad();
+            double pointDistance = distance(targetPos, transform.getCenter());
             float moveDistance = mouseMove.speed * deltaTime;
 
             if(pointDistance < moveDistance){
-                moveDelta.add(mouseMove.targetPosition);
+                moveDelta.add(targetPos);
                 moveDelta.sub(transform.getCenter());
                 mouseMove.targetPosition = null;
             }else{
                 moveDelta.add(new Vector2((float) (moveDistance * Math.cos(directionOfTravel)), (float) (moveDistance * Math.sin(directionOfTravel))));
             }
-
-            Gdx.app.log("MouseMoveSystem", "Movement: " + moveDelta);
-
         }
 
         return moveDelta;
