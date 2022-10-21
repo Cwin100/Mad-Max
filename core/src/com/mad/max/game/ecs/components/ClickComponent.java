3ec477567ee2cedcbed2826cd0b0b1a48f4ec3e6
Cole Winsor <ccwinsor@wpi.edu>
2022-10-20 19:34:18 -0600

    Optimized some imports

--- core/src/com/mad/max/game/ecs/components/ClickComponent.java
+++ core/src/com/mad/max/game/ecs/components/ClickComponent.java
@@ -1,10 +1,6 @@
 package com.mad.max.game.ecs.components;
 
 import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.graphics.Texture;
-
-import java.util.Arrays;
-import java.util.Optional;
 
 public class ClickComponent implements Component {
     public Runnable leftAction;
--- core/src/com/mad/max/game/ecs/systems/movesystems/IteratingMoveSystem.java
+++ core/src/com/mad/max/game/ecs/systems/movesystems/IteratingMoveSystem.java
@@ -6,6 +6,7 @@ import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Vector2;
 import com.mad.max.game.ecs.components.TransformComponent;
+import com.mad.max.game.ecs.components.movement.CarriedComponent;
 import com.mad.max.game.ecs.components.movement.CarryMoveComponent;
 
 public abstract class IteratingMoveSystem extends IteratingSystem {
